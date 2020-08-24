999d7b374c4dd0e79ad80dd747ac5a2682e28ec2
pung8146 <pung8146@naver.com>
2020-08-24 23:58:59 +0900

    유튜브 클론 onChange 기능 추가

---
+++ memo/4day.md
@@ -0,0 +1,15 @@
+useState 하면 자동완성되는게 리액트 훅의 기능 중 일부있다고 함
+
+import React , {useState} from 'react'; 임포트를 적는 방식들 ??
+
+처음state 은 빈공란으로 한다는게 그게 어떤것을 의미하는걸까?
+
+map이 무엇인지 왜 map을 쓸때 key가 있어야 에러메시지가 출력이 안되는지
+
+state 을 변경할때 옆에 있는 set변수명 을 사용해서 바꿔줄수있다
+
+Mongdb접속이 안되는데 StackOverflow 검색결과 starbucks 와이파이내에서 발생할수있다고한다.
+
+axios??
+
+파일을 라우트를 이용해서 올린다고하는데 라우트란게 무엇일까?
--- react-youtube-clone/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ react-youtube-clone/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,74 +1,136 @@
 // 예전에는 리액트 Hook 나오기전에클래스를 자주씀
 // 왜냐면 속도는 느리지만 많은 기능을 구현하기 쉬워서
 // Hook 이후로 퍼모펀스가 더좋은 함수형으로 바뀜
-import React from 'react';
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
-import Dropzone from 'react-dropzone';
+import React, { useState } from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+import Dropzone from "react-dropzone";
 
 const { TextArea } = Input;
 const { Title } = Typography;
 
+const PrivateOptions = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "public" },
+];
+
+const CategoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
 function VideoUploadPage() {
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
+
+  const onTitlChange = (e) => {
+    console.log(e.currentTarget);
+    setVideoTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategoryChange(e.currentTarget.value);
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", fiels[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config).then((response) => {
+      if (response.data.success) {
+      } else {
+        alert("비디오 업로드에 실패했습니다.");
+      }
+    });
+  };
+
   return (
-    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-      <div style={{ textAlign:'center', marginBottom:'2rem '}}>
-          <Title level={2}>Upload Video</Title>
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem " }}>
+        <Title level={2}>Upload Video</Title>
       </div>
 
-    <Form onSubmit>
-      <div style={{ display:'flex', justifyContent:'space-between' }}>
-        {/* Drop zone */}
-      
-        <Dropzone
-        onDrop
-        multiple
-        maxSize
-        >
-         {({ getRootProps, getInputProps }) =>(
-          <div style={{ width: '300px', height: '240px', border:'1px solid lightgray',
-          alignItems:'center', justifyContent: 'center'}} {...getRootProps()}>
-            <input {...getInputProps()} />
-            <Icon type="plus" style={{ fontSize: '3rem'}} />
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Drop zone */}
+
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: "3rem" }} />
+              </div>
+            )}
+          </Dropzone>
+
+          {/* Thumbnail */}
+          <div>
+            <img src alt />
           </div>
-         )}  
-            
-            </Dropzone>
-
-        {/* Thumbnail */}
-        <div>
-          <img src alt />
         </div>
-      </div>
 
-      <br/>
-      <br/>
-      <label>Title</label>
-       <Input
-          onChange
-          value
-        />
-      <br/>
-      <br/>
-      <label>Description</label>
-      <TextArea
-          onChange
-          value
-      />
-      <br />
-      <br />
-      
-      <select onChange>
-        <option key value></option>
-      </select>
+        <br />
+        <br />
+        <label>Title</label>
+        <Input onChange={onTitlChange} value={VideoTitle} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange={onDescriptionChange} value={Description} />
+        <br />
+        <br />
 
-      <Button type="primary" size="large" onClick>
-        Submit
-      </Button>
+        <select onChange={onPrivateChange}>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={(item, value)}>
+              {item.label}
+            </option>
+          ))}
+        </select>
 
-    </Form>
+        <br />
+        <br />
 
+        <select onChange={onCategoryChange}>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={(item, value)}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+
+        <br />
+        <br />
+
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default VideoUploadPage
+export default VideoUploadPage;
