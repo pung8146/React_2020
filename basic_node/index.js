6a5872873918280760b77a090544b6f520b6dfea
pung8146 <pung8146@naver.com>
2020-09-04 23:25:19 +0900

    NODE MON / body-parser 기능 적용

--- basic_node/index.js
+++ basic_node/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-from-urlencoded 분석
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 분석
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://pung8146:qwe123@reactstudy.kq8wb.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -9,8 +17,24 @@ mongoose.connect('mongodb+srv://pung8146:qwe123@reactstudy.kq8wb.mongodb.net/<db
 .catch(err => console.log(err))
 
 
+
 app.get('/', (req, res) => {
   res.send('Hello World! ~ 안녕하세요 ~')
+})
+
+app.post('/register', (req, res) => {
+
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
---
+++ memo/Body-parser-Dependency.md
@@ -0,0 +1,2 @@
+# Body-parser #
+설치방법 : npm install body-parser --save 
